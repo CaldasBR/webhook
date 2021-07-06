a640d376521e082ce1b16446ac5db2742c35cfb5
CaldasBR <fcaldas@gmail.com>
2021-07-06 09:42:50 -0300

    cria

--- criar_tabelas.py
+++ criar_tabelas.py
@@ -9,4 +9,4 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE roteamento (id INT AUTO_INCREMENT PRIMARY KEY, ownerIdentity VARCHAR(50), identity VARCHAR(100), contactIdentity VARCHAR(100), messageId VARCHAR(100), storageDate TIMESTAMP DEFAULT 0, category VARCHAR(50), action VARCHAR(50), stateId VARCHAR(100), stateName VARCHAR(50), previousStateId VARCHAR(100), previousStateName VARCHAR(50);")
+mycursor.execute("CREATE TABLE roteamento (id INT AUTO_INCREMENT PRIMARY KEY, ownerIdentity VARCHAR(50), identity VARCHAR(100), contactIdentity VARCHAR(100), messageId VARCHAR(100), storageDate TIMESTAMP, category VARCHAR(50), action VARCHAR(50), stateId VARCHAR(100), stateName VARCHAR(50), previousStateId VARCHAR(100), previousStateName VARCHAR(50));")
